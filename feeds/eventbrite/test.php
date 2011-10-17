3741f1ebb3665a896f234291747c42d5fd5dbebc
Pedro Dias <petermdias@gmail.com>
2011-10-17 04:58:20 +0100

    Testing...

--- feeds/eventbrite/test.php
+++ feeds/eventbrite/test.php
@@ -1,44 +1,46 @@
 <?php
+
 // load the API Client library
-include "Eventbrite.php"; 
+include "Eventbrite.php";
 
 // Initialize the API client
 //  Eventbrite API / Application key (REQUIRED)
 //   http://www.eventbrite.com/api/key/
 //  Eventbrite user_key (OPTIONAL, only needed for reading/writing private user data)
 //   http://www.eventbrite.com/userkeyapi
-$authentication_tokens = array('app_key'  => 'ECGUO5VWW7L637PDDW');
-$eb_client = new Eventbrite( $authentication_tokens );
+$authentication_tokens = array('app_key' => 'ECGUO5VWW7L637PDDW');
+$eb_client = new Eventbrite($authentication_tokens);
 
 // For more information about the features that are available through the Eventbrite API, see http://developer.eventbrite.com/doc/
-$events = $eb_client->event_search(array('country'=>'PT') );
+$events = $eb_client->event_search(array('country' => 'PT'));
 
 print_r($events);
 
 //mark-up the list of events that were requested 
-// render in html - ?>
+// render in html - 
+?>
 <style type="text/css">
-.eb_event_list_item{
-  padding-top: 20px;
-}
-.eb_event_list_title{
-  position: absolute;
-  left: 220px;
-  width: 300px;
-  overflow: hidden;
-}
-.eb_event_list_date{
-  padding-left: 20px;
-}
-.eb_event_list_time{
-  position: absolute;
-  left: 150px;
-}
-.eb_event_list_location{
-  position: absolute;
-  left: 520px;
-}
+    .eb_event_list_item{
+        padding-top: 20px;
+    }
+    .eb_event_list_title{
+        position: absolute;
+        left: 220px;
+        width: 300px;
+        overflow: hidden;
+    }
+    .eb_event_list_date{
+        padding-left: 20px;
+    }
+    .eb_event_list_time{
+        position: absolute;
+        left: 150px;
+    }
+    .eb_event_list_location{
+        position: absolute;
+        left: 520px;
+    }
 </style>
 
 <h1>My Event List:</h1>
-<?= Eventbrite::eventList( $events, 'eventListRow'); ?>
+<?= Eventbrite::eventList($events, 'eventListRow'); ?>
