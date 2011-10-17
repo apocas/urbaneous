ff6555b11a4e53f506982c14c15d0934867931e4
Pedro Dias <petermdias@gmail.com>
2011-10-17 03:46:56 +0100

    Revert e30a7eb8c2ccc97a8989d23a2a44a825ead90614^..HEAD

--- js/engine.js
+++ js/engine.js
@@ -4,21 +4,24 @@ var Engine = {
     
         $(document).ready(function()
         {
+    
+                    
+            function smartBackgroundImage(url,type){
+                //create an img so the browser will download the image:
+                $('<img />')
+                .attr('src', url)
+                .load(function(){ //attach onload to set background-image
+                    $('.header_bg').css('backgroundImage', 'url('+url+')' );
+                    $('.header_bg').css('backgroundPosition', type );
+                    $('.header_bg').fadeIn('slow');           
+                });          
+            }
+                    
+    
             Motions.init();
             Events.init();
     
         });
-    },
-    
-    smartBackgroundImage:function (url,type){
-        //create an img so the browser will download the image:
-        $('<img />')
-        .attr('src', url)
-        .load(function(){ //attach onload to set background-image
-            $('.header_bg').css('backgroundImage', 'url('+url+')' );
-            $('.header_bg').css('backgroundPosition', type );
-            $('.header_bg').fadeIn('slow');           
-        });          
     }
 
 }
--- js/events.js
+++ js/events.js
@@ -7,42 +7,42 @@ var Events = {
             var strl = str.toLowerCase();
             if(strl=="lisbon"){
                 $('.header_bg').fadeOut( 'slow', function(){    
-                    Engine.smartBackgroundImage('images/lisbon.jpg','center');
+                    smartBackgroundImage('images/lisbon.jpg','center');
                 });
             }
             if(strl=="london"){
                 $('.header_bg').fadeOut( 'slow', function(){    
-                    Engine.smartBackgroundImage('images/london.jpg','center');
+                    smartBackgroundImage('images/london.jpg','center');
                 });
             }
             if(strl=="sydney"){
                 $('.header_bg').fadeOut( 'slow', function(){    
-                    Engine.smartBackgroundImage('images/sydney.jpg','bottom');
+                    smartBackgroundImage('images/sydney.jpg','bottom');
                 });
             }
             if(strl=="new york"){
                 $('.header_bg').fadeOut( 'slow', function(){    
-                    Engine.smartBackgroundImage('images/newyork.jpg','top');
+                    smartBackgroundImage('images/newyork.jpg','top');
                 });
             }
             if(strl=="venezia"){
                 $('.header_bg').fadeOut( 'slow', function(){    
-                    Engine.smartBackgroundImage('images/venezia.jpg','bottom');
+                    smartBackgroundImage('images/venezia.jpg','bottom');
                 });
             }
             if(strl=="shanghai"){
                 $('.header_bg').fadeOut( 'slow', function(){    
-                    Engine.smartBackgroundImage('images/shanghai.jpg','center');
+                    smartBackgroundImage('images/shanghai.jpg','center');
                 });
             }
             if(strl=="kuala lumpur"){
                 $('.header_bg').fadeOut( 'slow', function(){    
-                    Engine.smartBackgroundImage('images/kualalumpur.jpg','center');
+                    smartBackgroundImage('images/kualalumpur.jpg','center');
                 });
             }
             if(strl=="san diego"){
                 $('.header_bg').fadeOut( 'slow', function(){    
-                    Engine.smartBackgroundImage('images/sandiego.jpg','0 -320px');
+                    smartBackgroundImage('images/sandiego.jpg','0 -320px');
                 });
             }
         });
