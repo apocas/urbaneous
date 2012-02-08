7d98bc7bf96d270339415d4296682a9586ee30a7
Pedro Dias <petermdias@gmail.com>
2012-02-08 00:21:15 +0000

    eventful

--- feeds/eventful/test.php
+++ feeds/eventful/test.php
@@ -1,4 +1,5 @@
 <?php
+
 require 'Services/Eventful.php';
 
 // Enter your application key here. (See http://api.eventful.com/keys/)
@@ -7,31 +8,30 @@ $app_key = 'PhSPfXsNh4bxtscN';
 $ev = new Services_Eventful($app_key);
 
 // Authentication is required for some API methods.
-$user     = $_REQUEST['user'];
+$user = $_REQUEST['user'];
 $password = $_REQUEST['password'];
 
-if ($user and $password)
-{
-  $l = $ev->login($user, $password);
-  
-  if ( PEAR::isError($l) )
-  {
-      print("Can't log in: " . $l->getMessage() . "\n");
-  }
+if ($user and $password) {
+    $l = $ev->login($user, $password);
+
+    if (PEAR::isError($l)) {
+        print("Can't log in: " . $l->getMessage() . "\n");
+    }
 }
 
 // All method calls other than login() go through call().
 $args = array(
-  'location' => 'Lisbon',
+    'location' => 'Lisbon',
+    'page_size' => '100',
+    'sort_order' => 'popularity'
 );
 $event = $ev->call('events/search', $args);
 
-if ( PEAR::isError($event) )
-{
+if (PEAR::isError($event)) {
     print("An error occurred: " . $event->getMessage() . "\n");
-    print_r( $ev );
+    print_r($ev);
 }
 
 // The return value from a call is a SimpleXMLElement object.
-print_r( $event );
+print_r($event);
 ?>
